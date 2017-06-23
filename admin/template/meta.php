7c9c27c77fb299cc237ee573211009e2d8c58d90
chimake <dev.chimaobi@gmail.com>
2017-06-23 20:23:01 +0100

    Admin

---
+++ admin/template/meta.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+<meta name="author" content="Coderthemes">
